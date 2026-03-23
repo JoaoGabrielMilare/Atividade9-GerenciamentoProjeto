bff325dcc1ed444accbe1fc51d2e89ad98cffc3f
JoaoGabrielMilare <168237403+JoaoGabrielMilare@users.noreply.github.com>
2026-03-23 18:52:39 -0300

    criando o Model

---
+++ Gerenciamento_Projetos/src/main/java/com/example/Gerenciamento_Projetos/Model/Projeto.java
@@ -0,0 +1,64 @@
+package com.example.Gerenciamento_Projetos.Model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "Projeto")
+@Table(name = "tb_projeto")
+public class Projeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "PROJETO_NOME")
+    private String nome;
+
+    @Column(name = "PROJETO_DATA_INICIO")
+    private LocalDate dataInicio;
+
+    @Column(name = "PROJETO_DATA_FIM")
+    private LocalDate dataFim;
+
+    public Projeto() {
+    }
+
+    public Projeto(Long id, String nome, LocalDate dataInicio, LocalDate dataFim) {
+        this.id = id;
+        this.nome = nome;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
